93ead9bd532d1a7d8630228c5672ed6152c65656
Ali Shan <92180842+Ali-shan-Adrees@users.noreply.github.com>
2026-05-12 10:05:34 +0500

    Rebuild .next with permanent dark mode, remove theme toggle

---
+++ .next/static/sGgnluggjAWPcSDVHJ5WV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/645-ab2e33da4d287561.js","static/chunks/pages/index-78a977055ea9fb56.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
